3008395ea58ad8dcf2e89bdbd6c01b46db018f76
Josh Andrews <jtandr3ws37@gmail.com>
2014-12-15 17:57:03 -0800

    MegaCommit. Modify .gitignore, add routes for directions & showLocations views, implement public directory.

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 // app.js 
 
-// require node modules
+// require node modules & load controllers
 var express = require ('express'),
 
 		app = express();
+
+//require js methods
 
 // set view engine to ejs
 app.set('view engine', 'ejs');
 
 app.get("/", function(req,res) {
 	res.render('index');
+})
+
+app.get("/searchMap", function(req,res) {
+	res.render('showLocation');
+})
+
+app.get("/directions", function(req,res) {
+	res.render('directions');
 })
 
 app.listen(3000, function (){
